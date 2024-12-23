c9d44e0e59cf77403a3b727cbf9d152bd48e5f38
ThawZinToe <thawzin@nimblehq.co>
2024-12-23 09:09:26 +0700

    Update min sdk and ui test for failed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.ptut.utulae"
-        minSdk = 30
+        minSdk = 25
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
