1b18304df6a2b8ad02bda586fa698edda26d699f
ThawZinToe <thawzin@nimblehq.co>
2024-12-23 09:50:24 +0700

    Add pre-commit hooks for spotlessApply

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,27 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.spotless) apply false
+}
+
+subprojects {
+    apply(plugin = "com.diffplug.spotless")
+    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+        kotlin {
+            target("**/*.kt")
+            targetExclude("${rootProject.layout.buildDirectory}/**/*.kt")
+            ktlint()
+            licenseHeaderFile(rootProject.file("spotless/copyright.kt"))
+        }
+
+        kotlinGradle {
+            target("*.gradle.kts")
+            ktlint()
+        }
+    }
+
+}
+
+tasks.register("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
